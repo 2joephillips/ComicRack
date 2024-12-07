0454299a67b1dd05e24ac8fa4d1c4caefbf9b0df
Joseph Phillips <joseph@josephphillips.co>
2024-12-07 17:33:10 -0500

    Initial set up hard coded comic location, and json storage

---
+++ ComicReader.Core/Comic.cs
@@ -0,0 +1,26 @@
+﻿namespace ComicReader.Core;
+
+public class Comic
+{
+  public string FilePath { get; set; }
+  public string FileName { get; set; }
+
+  public Comic(string filePath, string fileName)
+  {
+    FilePath = filePath;
+    FileName = fileName;
+  }
+}
+
+
+public class ComicRack
+{
+  private string comic_library_location = @"D:\Comics";
+  private string storage_file_location = @"D:\Comics\";
+  private string storage_file_name = "storage.json";
+
+  public string STORAGE_URL => storage_file_location + storage_file_name;
+
+  public string COMIC_LIBRARY_LOCACTION => comic_library_location;
+  public List<Comic> Comics { get; set; }
+}
