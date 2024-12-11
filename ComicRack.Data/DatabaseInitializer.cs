1270d189abfcde996c7a6a11a521130acf7faf63
Joseph Phillips <joseph@josephphillips.co>
2024-12-10 21:02:13 -0500

    simplify DB Seeding

--- ComicRack.Data/DatabaseInitializer.cs
+++ ComicRack.Data/DatabaseInitializer.cs
@@ -1,5 +1,5 @@
-﻿using ComicRack.Data.Data;
-using ComicReader.Data;
+﻿using ComicRack.Core;
+using ComicRack.Data.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace ComicReader.UI
@@ -13,7 +13,7 @@ namespace ComicReader.UI
             _context = context;
         }
 
-        public async Task<bool> InitializeDatabaseAsync()
+        public async Task<(bool Initilized, bool SetupCompleted)> InitializeDatabaseAsync()
         {
             try
             {
@@ -22,13 +22,31 @@ namespace ComicReader.UI
                 Console.WriteLine("Database migrated and up-to-date.");
 
                 // Seed initial settings
-                return await DBSeeder.SeedInitialSettingsAsync(_context);
+                return await SeedInitialSettingsAsync(_context);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error during migration: {ex.Message}");
                 throw;
             }
+        }
+
+
+        private static async Task<(bool Initilized, bool SetupCompleted)> SeedInitialSettingsAsync(ApplicationDbContext context)
+        {
+            var anySettings = context.Settings.Any();
+            if (anySettings)
+            {
+                var setting = await context.Settings.FirstOrDefaultAsync(s => s.Key == "SetUpComplete");
+                var setupComplete = setting != null && setting.Value == "true";
+                return (false, setupComplete);
+            };
+            context.Settings.AddRange(
+                new Setting { Key = "Theme", Value = "Lite" },
+                new Setting { Key = "SetUpComplete", Value = "false" }
+                );
+            await context.SaveChangesAsync();
+            return (true, false);
         }
     }
 }
